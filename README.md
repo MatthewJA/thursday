f7684fd94fc5bab7481b0a6b664582c17a59331b
josh-marsh <35943151+josh-marsh@users.noreply.github.com>
2018-04-17 13:23:41 +1000

    API overhaul (#33)
    * API overhaul
    
    * readme corrections
    
    * suggested fixes
    
    * forgot to save for last commit
    
    * Readme, doctrings, and whitespace fixes
    
    * added class-wise balancing and shuffling
    
    * removed confusion matrix for HOGNet

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Classifying radio galaxies as Fanaroff-Riley Type I (FR-I) and Fanaroff-Riley Type II (FR-II).
 
 
+
 ## Input files
 
 You will need
@@ -11,20 +12,20 @@ You will need
 
 ## Running the code
 
-1. Run `fri-frii-download.ipynb` to get all FRI/FRII samples in fri-cat, frii-cat, and FIRST. Data will be stored in as `data.h5` in (current_directory/data/data.h5)
-2. Use the get_data function in `format_data.py` to generate the training and testing data (train_x, train, y, test_x, test_y) from `data.h5` and `asu.tsv`.
-3. Use the `data_gen` function in `format_data.py` to create the data generator.
-4. If using both sklearn and hognet and
+1. Run `fri-frii-download.ipynb` to download all FRI/FRII samples in fri-cat, frii-cat, and FIRST. Data will be stored as `data.h5` in (current_directory/data/data.h5)
+2. Use the get_data function from `format_data.py` to generate the training and testing data indices from `data.h5` and `asu.tsv`.
+3. Open `data.h5`  as `data` and and use indices to select training and test images from `data['images']` and `data['labels']`
+4. Use the `data_gen` function in `format_data.py` to construct the data generator.
+5. Instantiate the`HOGNnet` and/or `SklearnModel` from `models.py`. Both take `datagen` and `seed` as arguments. `SklearnModel` takes additional arguments like `Model` (the sklearn classifier being used), `nb_augment` (factor data is increased via augmentations), as well as any number of parameters specific to `Model`. `HOGNnet` takes `batch_size`, `steps_per_epoch`, `max_epoch`, and `patience` as additional arguments.
+6. Use the `fit` method to train the model. Both have `train_x` and `train_y` as inputs.
+7. Optional: If training times are long or you want to store your trained model, use the save method to save the model to disk (`SklearnModel` as a `.pk` file and `HOGnet` as a `.h5` file) 
+8. Optional: Load model from disk with the `load` method.
+9. Use `predict` method to predict class labels and `predict_proba` class probabilities for unseen samples (`test_x`). 
 
-Instantiate `HognetModel` class in `model_hognet.py`. Do the following with `HognetModel` class methods: Use `build` method to construct the model, and the `train method` to train the model, and then the `load` method to return the fully trained model (This has not yet been pushed due to some highly irritating tensorflow issues).
-5. Instantiate desired `SklearnModel` class in `models.py`. Use `train` method to build and train the sklearn classifier, and then the `load` method to return the fully trained model. If comparing a sklearn model to hognet, use sklearn after hognet (keras uses a generator and stops training when loss plateaus, and thus the number of epochs for which the data is generated is unknown)
-5. Use trained models to make predictions.
 
 ![alt text](https://github.com/josh-marsh/thursday/blob/reload/Basic/files/flow.jpg)
 
 ## Example
 
-A working example can be found in [`example_usage.ipynb`](final%20release/example_usage.ipynb). 
+A working example can be found at [`example_usage.ipynb`](thursday/example_usage.ipynb). 
 
-## Remarks
-While our implementation is functioning properly, it will not produce a meaningful classifier. Due to poorly cited data origins in certain papers, we are currently missing all bar one of the FR-II samples found in `asu.tsv`. To make the script work, we increased the radius dramatically when matching catalogs, adding some FRIs into the FR-II set. 
--- thursday/format_data.py
+++ thursday/format_data.py
@@ -13,22 +13,22 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import np_utils
 import matplotlib.pyplot as plt
 import numpy as np
-from skimage.transform import resize
+from skimage.transform import resize as skimage_resize
 from sklearn.utils import shuffle
 
 
-def rescale_imgs(images, dim):
+def resize(images, dim=None):
     """Rescales array of images to specified dimensions."""
     size = images.shape[0]
     imgs = np.zeros((size, dim, dim))
 
     for i in range(size):
-        imgs[i, :, :] = resize(images[i, :, :], (dim, dim))
+        imgs[i, :, :] = skimage_resize(images[i, :, :], (dim, dim))
 
-    return np.expand_dims(imgs, axis=3)
+    return imgs
 
 
-def get_data(first_path, second_path, dim=256, seed=0, conf_matrix=True, flip_train_test=False):
+def get_data(fr_data_path, aniyan_path, seed=0):
     """Formats data into training and testing sets.
     
     Takes a base dataset and a second dataset in specific formats. 
@@ -41,34 +41,23 @@ def get_data(first_path, second_path, dim=256, seed=0, conf_matrix=True, flip_tr
     base dataset are formated into a balanced training set and the 
     extracted images are formatted into a balanced testing set.
     
-    
     # Arguments
-        first_path: File path of the base dataset. Must be a h5py file 
+        fr_data_path: File path of the base dataset. Must be a h5py file 
             of the exact format as the one used for this script 
             (see https://github.com/josh-marsh/thursday)
-        second_path: File path of the second dataset. Must be a .tsv file 
+        aniyan_path: File path of the second dataset. Must be a .tsv file 
             of the exact format as the one used for this script 
             (see https://github.com/josh-marsh/thursday)
-        dim: Length and width that the images are rescaled two as an 
-            integer.
-        seed: Random seed. All our tests used 0.
-        conf_matrix: If true, train_y and test_y are returned as confusion
-            matrices. If False, an array of bools. Defaults to True
-        flip_train_test: If true, the training and testing data are reversed.
-            That is, images in the second dataset are used for training and
-            vice versa. Defaults to False
-        
-        
+        seed: Seed value to consistently initialize the random number 
+                generator.  
+       
     # Returns
-        Training and testing data in the form:
-        (train_x, train_y, test_x, test_y). There are an equal number of 
-        FRIs and FRIIs in each set, with the rest discarded. The shapes 
-        of train_x and test_x are (nb_samples, dim, dim, 1).
-
+        Locations training and testing data as Boolean arrays with shape 
+        [samples,]. There are an equal number of FRIs and FRIIs in each 
+        set, with the rest discarded.
     """
     # Loading data
-    with h5py.File(first_path, 'r') as data:
-        images = data['images'].value
+    with h5py.File(fr_data_path, 'r') as data:
         labels = data['labels'].value.astype(bool)
         
         fri_data = data['fri_data']
@@ -85,77 +74,63 @@ def get_data(first_path, second_path, dim=256, seed=0, conf_matrix=True, flip_tr
         frii_c = np.hstack((frii_ra, frii_de))
 
     # Opening Catalogues
-    asu = asc.read(second_path).to_pandas()
+    aniyan = asc.read(aniyan_path).to_pandas()
     
     # Splitting classes
-    fri_images = images[~labels]
-    frii_images = images[labels]
+    fri_ind  = ~labels
+    frii_ind = labels
      
     # Coverting coordinates of wildly varying formats to SkyCoords
-    asu_ra = asu.iloc[2:]['RAJ2000']
-    asu_dec = asu.iloc[2:]['DEJ2000']
+    aniyan_ra = aniyan.iloc[2:]['RAJ2000']
+    aniyan_dec = aniyan.iloc[2:]['DEJ2000']
     
-    coord_asu = SkyCoord(ra=asu_ra, dec=asu_dec, unit=('hourangle', 'deg'))
+    coord_aniyan = SkyCoord(ra=aniyan_ra, dec=aniyan_dec, unit=('hourangle', 'deg'))
     coord_fri = SkyCoord(ra=fri_c[:, 0], dec=fri_c[:, 1], unit=('hourangle', 'deg'))
     coord_frii = SkyCoord(ra=frii_c[:, 0], dec=frii_c[:, 1], unit=('deg', 'deg'))
    
-    # Finding nearest FRI's and FRIIs to each object in ASU
-    idx_i, sep_i, _i = coord_fri.match_to_catalog_sky(coord_asu)
-    idx_ii, sep_ii, _ii = coord_frii.match_to_catalog_sky(coord_asu)
+    # Finding nearest FRI's and FRIIs to each object in aniyan
+    idx_i, sep_i, _i = coord_fri.match_to_catalog_sky(coord_aniyan)
+    idx_ii, sep_ii, _ii = coord_frii.match_to_catalog_sky(coord_aniyan)
 
     # For sources within our set threshold, we assume that they are the same object.
-    fri_in_asu = sep_i <= 5 * u.arcsec
-    frii_in_asu = sep_ii <= 5 * u.arcsec
+    fri_in_aniyan = sep_i <= 5 * u.arcsec
+    frii_in_aniyan = sep_ii <= 5 * u.arcsec
 
-    # Extacting FR images that are in asu
-    asu_fri = fri_images[fri_in_asu]
-    asu_frii = frii_images[frii_in_asu]
+    fri_leftover = ~fri_in_aniyan
+    frii_leftover = ~frii_in_aniyan
 
-    leftover_fri = fri_images[~fri_in_asu]
-    leftover_frii = frii_images[~frii_in_asu]
- 
-    # Generating Labels
-    asu_fri_labels = np.zeros(asu_fri.shape[0], dtype=bool)
-    asu_frii_labels = np.ones(asu_frii.shape[0], dtype=bool)
+    # Fectching indices
+    fri_in_aniyan = np.where(fri_in_aniyan)[0]
+    frii_in_aniyan = np.where(frii_in_aniyan)[0]
 
-    leftover_fri_labels = np.zeros(leftover_fri.shape[0], dtype=bool)
-    leftover_frii_labels = np.ones(leftover_frii.shape[0], dtype=bool)
+    fri_leftover = np.where(fri_leftover)[0]
+    frii_leftover = np.where(frii_leftover)[0]
 
-    # Combining FRI's and FRII's and shuffling
-    asu_fr = np.vstack((asu_fri, asu_frii))
-    asu_fr_labels = np.concatenate((asu_fri_labels, asu_frii_labels), axis=0) 
+    # Finding the number of samples in the smallest fr class
+    # So there are an equal amount of each class in each set
+    aniyan_cut = np.min(np.array([fri_in_aniyan.shape[0], frii_in_aniyan.shape[0]]))
+    leftover_cut = np.min(np.array([fri_leftover.shape[0], frii_leftover.shape[0]]))
 
-    leftover_fr = np.vstack((leftover_fri, leftover_frii))
-    leftover_fr_labels = np.concatenate((leftover_fri_labels, leftover_frii_labels), axis=0)             
-    # Shuffling
-    asu_fr_, asu_fr_labels_ = shuffle(asu_fr, asu_fr_labels, random_state=seed)
-    leftover_fr_, leftover_fr_labels_ = shuffle(leftover_fr, leftover_fr_labels, random_state=seed) 
+    # Shuffling before discarding
+    fri_in_aniyan = shuffle(fri_in_aniyan, random_state=seed)
+    frii_in_aniyan = shuffle(frii_in_aniyan, random_state=seed)
+    fri_leftover = shuffle(fri_leftover, random_state=seed)
+    frii_leftover = shuffle(frii_leftover, random_state=seed)
 
-    # Setting training and testing sets
-    if not flip_train_test:
-        train_x = asu_fr_
-        train_y = asu_fr_labels_
-
-        test_x = leftover_fr_
-        test_y = leftover_fr_labels_
-
-    else:
-        train_x = leftover_fr_
-        train_y = leftover_fr_labels_
-
-        test_x = asu_fr_
-        test_y = asu_fr_labels_
-
-    # Rescaling Images
-    train_x = rescale_imgs(train_x, dim)
-    test_x = rescale_imgs(test_x, dim)
-
-    # Convert class vectors to binary class matrices (for Keras Dense)
-    if conf_matrix:
-        train_y = np_utils.to_categorical(train_y, 2)
-        test_y = np_utils.to_categorical(test_y, 2)
+    # Discarding samples
+    fri_in_aniyan = np.sort(fri_in_aniyan[:aniyan_cut], axis=0)
+    frii_in_aniyan = np.sort(frii_in_aniyan[:aniyan_cut], axis=0)
+    fri_leftover = np.sort(fri_leftover[:leftover_cut], axis=0)
+    frii_leftover = np.sort(frii_leftover[:leftover_cut], axis=0)
     
-    return train_x, train_y, test_x, test_y    
+    aniyan = np.concatenate((fri_in_aniyan, frii_in_aniyan), axis=0)
+    leftover = np.concatenate((fri_leftover, frii_leftover), axis=0)
+
+    # Training and testing sets
+    train_i = leftover
+    test_i = aniyan
+
+    return train_i, test_i
 
 def add_noise(image):
         """ Adds tiny random Gaussian Noise to each image. 
@@ -165,16 +140,30 @@ def add_noise(image):
          occurred when sections of an image had pixel intensity values 
          of zero due to formatting errors, resulting in intensity 
          gradients of zero that broke the arctan2 function in our custom
-         keras layer
+         keras layer.
          """
         image += 10e-10 * np.random.randn(image.shape[0], image.shape[1], 1)
+        
         return image
+    
+def augment_data(rotation_range=180, zoom_range=0.2, shift_range=0.0, flip=True):
+    """ Initializes data generator.
 
-    
-def data_gen(rotation_range=180, zoom_range=0.2, shift_range=0.0, flip=True):
-    """ Initializes data generator."""
-    
-    # Defining Data Generator
+    Arguments:
+        rotation_range: Int. Degree range for random rotations.
+        shift_range: Float (fraction of total width). Range for random 
+            horizontal and vertical shifts.
+        zoom_range: Float or [lower, upper]. Range for random zoom. If a 
+            float, [lower, upper] = [1-zoom_range, 1+zoom_range]
+        horizontal_flip: Boolean. Randomly flip inputs horizontally.
+        fill_mode: One of {"constant", "nearest", "reflect" or "wrap"}. Default 
+            is 'nearest'. Points outside the boundaries of the input are filled
+            according to the given mode
+        preprocessing_function: function that will be implied on each input. 
+            The function will run after the image is resized and augmented. The 
+            function should take one argument: one image (Numpy tensor with 
+            rank 3), and should output a Numpy tensor with the same shape.
+    """
     datagen = ImageDataGenerator(
                 rotation_range=rotation_range,
                 zoom_range=zoom_range,
@@ -186,77 +175,3 @@ def data_gen(rotation_range=180, zoom_range=0.2, shift_range=0.0, flip=True):
                 preprocessing_function=add_noise)
     
     return datagen
-
-
-def data_pregenerate(images, labels, datagen, batch_size, nb_epoch, seed):
-    """Pre-generates data for Sklearn models.
-    
-    Using base set of images, a random combination is augmentations within
-    a defined range is applied to generate a new batch of data.
-    
-    
-    # Arguments
-        images: Array of images with shape (samples, dim, dim, 1)
-        labels: Array of labels
-        datagen: The data generator outputed by the data_gen function.
-        batch_size: Number of sample per batch.
-        nb_epoch: The number of epochs to generate for.
-        seed: Random seed. All our tests used 0.
-        
-    # Returns
-        Array of augmented images and their corresponding labels.
-
-    """
-    samples = images.shape[0]
-    
-    # the .flow() command below generates batches of randomly transformed images
-    gen = datagen.flow(images, labels, batch_size=batch_size, seed=seed)
-
-    # Generate empty data arrays
-    pro_images = np.zeros((images.shape[0] * nb_epoch, images.shape[1],
-                           images.shape[2], 1))
-    pro_labels = np.zeros((labels.shape[0] * nb_epoch))
-    
-    for epoch in range(1, nb_epoch+1):
-        batch = 1
-        b = batch_size
-        b_start = samples * (epoch-1)
-
-        for X_batch, Y_batch in gen:
-            if batch < (samples / b):
-                cut_start = b_start + b * (batch-1)
-                cut_stop = b_start + batch * b
-                
-                pro_images[cut_start:cut_stop, :, :, :] = X_batch
-                pro_labels[cut_start:cut_stop] = Y_batch
-                
-            elif batch == samples // b:
-                break
-
-            else:
-                cut_start = b_start + b * (batch-1)
-                cut_stop = b_start + b * (batch-1) + X_batch.shape[0] % b
-                
-                pro_images[cut_start:cut_stop, :, :, :] = X_batch
-                pro_labels[cut_start:cut_stop] = Y_batch
-                break
-
-            batch += 1
-    
-    pro_images = np.reshape(pro_images, (np.shape(pro_images)[0], -1))
-    
-    return pro_images, pro_labels
-
-
-def data_pregen(train_x, train_y, test_x, test_y, 
-                datagen, batch_size, nb_epoch, seed):
-    """Applies data_pregenerate to both the training and testing test"""
-    train_x, train_y = data_pregenerate(images=train_x, labels=train_y,
-                                        datagen=datagen, batch_size=batch_size,
-                                        nb_epoch=nb_epoch, seed=seed)
-    
-    test_x, test_y  = data_pregenerate(images=test_x, labels=test_y, 
-                                       datagen=datagen, batch_size=math.ceil(batch_size / 5),
-                                       nb_epoch=nb_epoch, seed=seed)
-    
-    return train_x, train_y, test_x, test_y
