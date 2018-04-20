06ae6f2ea0738245d35d7a37cd0e0f4d9a67d841
josh-marsh <josh.j.marsh@gmail.com>
2018-04-20 16:57:41 +1000

    suggested fixes

--- thursday/experiments.py
+++ thursday/experiments.py
@@ -1,18 +1,15 @@
 import h5py
-
 import numpy as np
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.naive_bayes import GaussianNB
 
-
 from format_data import augment_data, get_data, get_fr_data, resize
 from models import SklearnModel, HOGNet
 
-print ("Experiment 1: Classifing FR-I vs FR-II")
-print ("Training on two thirds of class-wise balenced FR Data")
-print ("Testing on a third of class-wise balenced FR Data")
+print("Experiment 1: Classifing FR-I vs FR-II")
+print("Training on two thirds of class-wise balenced FR Data")
+print("Testing on a third of class-wise balenced FR Data")
 
 # Setting Seed
 seed = 0
@@ -26,25 +23,21 @@ train_i, test_i = get_fr_data("data/data.h5", split_ratio=(1/3), seed=seed)
 with h5py.File("data/data.h5", 'r') as data:
     train_x = np.asarray(data['images'])[train_i]
     train_y = np.asarray(data['labels'])[train_i]
-    
-    # Formatting images
-    train_x = resize(train_x, dim=256)
-    train_x = np.expand_dims(train_x, axis=3)
-
-with h5py.File("data/data.h5", 'r') as data:
     test_x = np.asarray(data['images'])[test_i]
     test_y = np.asarray(data['labels'])[test_i]
     
     # Formatting images
+    train_x = resize(train_x, dim=256)
     test_x = resize(test_x, dim=256)
+    train_x = np.expand_dims(train_x, axis=3)
     test_x = np.expand_dims(test_x, axis=3)
 
+    
 # Instantiating Classifiers
 random_forest = SklearnModel(RandomForestClassifier, datagen=datagen, nb_augment=10, seed=seed)
-#logistic_reg = SklearnModel(LogisticRegressionCV, datagen=datagen, nb_augment=5, seed=seed)
 naive_bayes = SklearnModel(GaussianNB, datagen=datagen, nb_augment=5, seed=seed)
 hognet = HOGNet(datagen=datagen, batch_size=64, steps_per_epoch=50, 
-                                max_epoch=100, patience=5, gap=2, seed=seed)
+                max_epoch=100, patience=5, gap=2, seed=seed)
 
 classifiers = [random_forest, naive_bayes, hognet]
 
